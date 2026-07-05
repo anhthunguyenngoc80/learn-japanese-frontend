b90d79d774d783f4357d93322951022f57e47405
anhthunguyenngoc <anhthunguyenngoc80@gmail.com>
2026-07-06 00:14:52 +0700

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
   ],
 })
