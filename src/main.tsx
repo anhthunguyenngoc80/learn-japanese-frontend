a126baf3dccf5b18a91b5f7a3389f0bfa520cd9b
anhthunguyenngoc <anhthunguyenngoc80@gmail.com>
2026-07-18 10:56:32 +0700

    add log err

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,3 @@
-window.onerror = function (message, source, line) {
-  alert(`
-${message}
-
-${source}:${line}
-    `);
-};
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
